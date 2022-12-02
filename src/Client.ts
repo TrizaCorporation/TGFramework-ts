829b7211aed97a79c44d05311b079bbffa9a5b1b
CodedJimmy <46508035+CodedJimmy@users.noreply.github.com>
2022-12-02 21:11:17 +0000

    Work On Client

--- src/Client.ts
+++ src/Client.ts
@@ -1,3 +1,32 @@
-export default function(){
+import { DataStoreService } from "@rbxts/services"
 
+interface Controller {
+    Name: string,
+    Init?: Callback,
+    Start?: Callback
+}
+
+export default class FrameworkClient {
+    Started: boolean
+    Controllers: Map<string, Controller>
+    constructor() {
+        this.Controllers = new Map()
+        this.Started = false
+    }
+
+    Start(){
+        for (const [Controller, Data] of this.Controllers){
+            if (Data.Init){
+                Data.Init()
+            }
+            if (Data.Start){
+                task.spawn(Data.Start)
+            }
+        }
+    }
+
+    CreateController(controllerName: string, data: Controller){
+        assert(!this.Started, "Controllers can't be created after TGFramework has started.")
+        this.Controllers.set(controllerName, data)
+    }
 }

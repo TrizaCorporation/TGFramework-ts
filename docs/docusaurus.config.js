d411d2e9db488c4f6d0d688a5a8cae02929a2ce8
CodedJimmy <46508035+CodedJimmy@users.noreply.github.com>
2023-01-31 17:38:35 +0000

    Add Docs

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,153 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+const path = require("path")
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'TGFramework-ts',
+  tagline: 'A Networking Framework For roblox-ts',
+  url: 'https://tgframework-ts.docs.triza.dev',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'TrizaCorporation', // Usually your GitHub org/user name.
+  projectName: 'TGFramework-ts-docs', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  plugins: [
+    [
+      'docusaurus-plugin-typedoc-api',
+      {
+        projectRoot: require('path').resolve(__dirname, '..'),
+        packages: ["."],
+        debug: true,
+        readmes: false,
+      },
+    ],
+    [
+      require.resolve('docusaurus-lunr-search'),
+      {
+      },
+    ]
+  ],
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        defaultMode: "dark"
+      },
+      navbar: {
+        title: 'TGFramework-ts',
+        logo: {
+          alt: 'Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'Intro',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            to: "api",
+            position: 'left',
+            label: 'API',
+          },
+          //{ to: '/blog', label: 'Blog', position: 'left' },
+          {
+            href: 'https://github.com/CodedJimmy/nodesignals',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'API',
+                to: '/api',
+              },
+            ],
+          },
+          {
+            title: 'Creator\'s Socials',
+            items: [
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/CodedJimmy',
+              }
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              /*
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              */
+              {
+                label: 'GitHub',
+                href: 'https://github.com/CodedJimmy/nodesignals',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} T:Riza Corporation, Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
